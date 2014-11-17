ab721fb18590c6844d15cd182161d69b47b26177
mala23 <mail@mala23.me>
2014-11-17 20:43:38 +0000

    list out matchers

---
+++ spec/matchers_spec.rb
@@ -0,0 +1,12 @@
+describe "Matchers" do
+  it "asserts on equality"
+  it "asserts on mathematical operators"
+  it "asserts on matching a regular expression"
+  it "asserts on types and classes"
+  it "asserts on truthiness"
+  it "expects errors"
+  it "expects throws"
+  it "asserts on predicates"
+  it "asserts on collections"
+  it "negates asserts"
+end
