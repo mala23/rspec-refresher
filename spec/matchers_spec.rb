1cad313c982ee896348abba8bf05fcfc2e3888a9
mala23 <mail@mala23.me>
2014-11-18 08:57:21 +0000

    first five matchers passing

--- spec/matchers_spec.rb
+++ spec/matchers_spec.rb
@@ -1,12 +1,31 @@
 describe "Matchers" do
-  it "asserts on equality"
-  it "asserts on mathematical operators"
-  it "asserts on matching a regular expression"
-  it "asserts on types and classes"
+
+  it "asserts on equality" do
+    number = 3
+    expect(number).to eq 3
+  end
+
+  it "asserts on mathematical operators" do
+    number = 5
+    expect(number).to be >= 2
+  end
+
+  it "asserts on matching a regular expression" do
+    mail = "foo@bar.io"
+    regular_expression = /^\w+@\w+\.[a-z]{2,4}$/
+    expect(mail).to match regular_expression
+  end
+  
+  it "asserts on types and classes" do
+    object = 2.3
+    expect(object).to be_a Numeric
+  end
+
   it "asserts on truthiness"
   it "expects errors"
   it "expects throws"
   it "asserts on predicates"
   it "asserts on collections"
   it "negates asserts"
+
 end
